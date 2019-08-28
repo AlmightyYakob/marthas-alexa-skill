8a05df0f2cf74542c0cddf33e6ab648501e6f0d4
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-08-28 19:58:59 -0400

    Update formatting

--- lambda/custom/constants.js
+++ lambda/custom/constants.js
@@ -8,53 +8,55 @@ export const SKILL_TITLE_NAME = 'Martha\'s Daily Flavors';
 export const BUCKET_NAME = 'marthas-flavor-skill';
 export const CALENDAR_KEY = 'flavor-cycle.json';
 export const CALENDAR_BUCKET_PARAMS = {
-    Bucket: BUCKET_NAME,
-    Key: CALENDAR_KEY,
+  Bucket: BUCKET_NAME,
+  Key: CALENDAR_KEY,
 };
 
-export const DANDEE_SSML_STRING = `<phoneme alphabet="ipa" ph="ˈdæn.di">Dandee</phoneme>`;
-export const CREME_SSML_STRING = `<phoneme alphabet="ipa" ph="kɹiːm">Creme</phoneme>`;
+export const DANDEE_SSML_STRING = '<phoneme alphabet="ipa" ph="ˈdæn.di">Dandee</phoneme>';
+export const CREME_SSML_STRING = '<phoneme alphabet="ipa" ph="kɹiːm">Creme</phoneme>';
 
 export const WEEKDAYS = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
 
 export const MONTHS = [
-    'January',
-    'Febuary',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 
 export const WORD_MAPPINGS = [
-    {
-        search: /Rasp(\s)/ig,
-        replace: 'Raspberry$1',
-    },
-    {
-        search: /Choc(\s)/ig,
-        replace: 'Chocolate$1',
-    },
-    {
-        search: /Cr(?:e|è)me(\s)/ig,
-        replace: 'Cream$1'
-    },
-    {
-        search: /(P\/B)/ig,
-        replace: 'Peanut Butter'
-    }
+  {
+    search: /Rasp(\s)/ig,
+    backwardSearch: /Raspberry(\s)/ig,
+    replace: 'Raspberry$1',
+    backwardReplace: 'Rasp$1',
+  },
+  {
+    search: /Choc(\s)/ig,
+    replace: 'Chocolate$1',
+  },
+  {
+    search: /Cr(?:e|è)me(\s)/ig,
+    replace: 'Cream$1',
+  },
+  {
+    search: /(P\/B)/ig,
+    replace: 'Peanut Butter',
+  },
 ];
--- lambda/custom/utils.js
+++ lambda/custom/utils.js
@@ -2,46 +2,61 @@ import AWS from 'aws-sdk';
 import moment from 'moment-timezone';
 
 import {
-    MS_PER_DAY,
-    PINNED_EPOCH_TIME_MS,
-    PINNED_DAY_CYCLE,
-    CALENDAR_BUCKET_PARAMS,
-    WORD_MAPPINGS,
+  MS_PER_DAY,
+  PINNED_EPOCH_TIME_MS,
+  PINNED_DAY_CYCLE,
+  CALENDAR_BUCKET_PARAMS,
+  WORD_MAPPINGS,
 } from './constants';
 
 
 export const getDate = (date, day) => {
-    const today = moment.tz('America/New_York').hours(0).minutes(0).seconds(0).milliseconds(0);
-    if (!date && !day) return today.toDate();
+  const today = moment
+    .tz('America/New_York')
+    .hours(0)
+    .minutes(0)
+    .seconds(0)
+    .milliseconds(0);
 
-    const returnDate = date
-        ? moment.tz(date, 'America/New_York')
-        : moment(today).day(day);
+  if (!date && !day) return today.toDate();
 
-    if (day && today.day() >= returnDate.day()) returnDate.day(returnDate.day() + 7);
+  const returnDate = date
+    ? moment.tz(date, 'America/New_York')
+    : moment(today).day(day);
 
-    return returnDate.toDate();
-}
+  if (day && today.day() >= returnDate.day()) returnDate.day(returnDate.day() + 7);
+
+  return returnDate.toDate();
+};
 
 export const getCurrentCycle = (currentDate) => {
-    const pinnedDate = getDate(PINNED_EPOCH_TIME_MS);
+  const pinnedDate = getDate(PINNED_EPOCH_TIME_MS);
 
-    console.log('CURRENT DATE', currentDate);
-    console.log('PINNED DATE', pinnedDate);
-    console.log('RAW DIFF', (currentDate - pinnedDate)/MS_PER_DAY);
+  console.log('CURRENT DATE', currentDate);
+  console.log('PINNED DATE', pinnedDate);
+  console.log('RAW DIFF', (currentDate - pinnedDate) / MS_PER_DAY);
 
-    const dayDifference = Math.ceil((currentDate - pinnedDate)/MS_PER_DAY);
-    return (dayDifference + PINNED_DAY_CYCLE) % 18;
+  const dayDifference = Math.ceil((currentDate - pinnedDate) / MS_PER_DAY);
+  return (dayDifference + PINNED_DAY_CYCLE) % 18;
 };
 
 export const getCalendar = async () => {
-    const s3 = new AWS.S3();
-    return JSON.parse((await s3.getObject(CALENDAR_BUCKET_PARAMS).promise()).Body.toString());
+  const s3 = new AWS.S3();
+  return JSON.parse((await s3.getObject(CALENDAR_BUCKET_PARAMS).promise()).Body.toString());
 };
 
 export const replaceStrings = (string) => {
-    let newString = string;
+  let newString = string;
 
-    WORD_MAPPINGS.forEach(word => newString = newString.replace(word.search, word.replace));
-    return newString;
+  WORD_MAPPINGS.forEach((word) => { newString = newString.replace(word.search, word.replace); });
+  return newString;
+};
+
+export const getFlavorNextCycle = async (flavor, currentCycle) => {
+  const calendar = await getCalendar();
+  console.log('calendar', calendar);
+
+  return calendar.findIndex(day => day
+    .map(x => x.toLowerCase())
+    .indexOf(flavor.toLowerCase(), currentCycle));
 };
